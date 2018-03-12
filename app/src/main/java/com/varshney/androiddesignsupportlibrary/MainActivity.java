f73a50c98ddc412c3d6e3ee8ace7ad9bcff67b6e
sherlock07 <yvarshney0707@gmail.com>
2018-03-13 02:44:44 +0530

    Fragment with viewPager added

--- app/src/main/java/com/varshney/androiddesignsupportlibrary/MainActivity.java
+++ app/src/main/java/com/varshney/androiddesignsupportlibrary/MainActivity.java
@@ -1,10 +1,17 @@
 package com.varshney.androiddesignsupportlibrary;
 
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.Adapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,7 +29,50 @@ public class MainActivity extends AppCompatActivity {
         tabs.addTab(tabs.newTab().setText("Tab 3"));
 
         ViewPager viewPager = findViewById(R.id.viewpager);
+        setUpViewPager(viewPager);
 
+        //After Executing next line tabs name changes from Tab1,Tab2,Tab3 to list,tile,card respectively.
+        tabs.setupWithViewPager(viewPager);
 
     }
+    private void setUpViewPager(ViewPager viewPager)
+    {
+        Adapter adapter = new Adapter(getSupportFragmentManager());
+        adapter.addFragment(new ListContentFragment(), "List");
+        adapter.addFragment(new TileContentFragment(), "Tile");
+        adapter.addFragment(new CardContentFragment(), "Card");
+        viewPager.setAdapter(adapter);
+    }
+
+    static class Adapter extends FragmentPagerAdapter{
+
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public Adapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+
+
 }
