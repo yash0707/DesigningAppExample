247887d4957966ffa568978cc1c50069becd871e
sherlock07 <yvarshney0707@gmail.com>
2018-03-13 02:16:06 +0530

    All Layout Ready To code now

---
+++ app/src/main/java/com/varshney/androiddesignsupportlibrary/MainActivity.java
@@ -0,0 +1,28 @@
+package com.varshney.androiddesignsupportlibrary;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+        TabLayout tabs = (TabLayout) findViewById(R.id.tabs);
+        tabs.addTab(tabs.newTab().setText("Tab 1"));
+        tabs.addTab(tabs.newTab().setText("Tab 2"));
+        tabs.addTab(tabs.newTab().setText("Tab 3"));
+
+        ViewPager viewPager = findViewById(R.id.viewpager);
+
+
+    }
+}
